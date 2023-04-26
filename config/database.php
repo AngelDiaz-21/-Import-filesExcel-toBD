cf44973cb4291e7cedefe894c64f806591885ed3
Angel <angelSistemas21@gmail.com>
2023-04-25 19:51:05 -0600

    remove: Conexión innecesaria

--- config/database.php
+++ config/database.php
@@ -83,26 +83,6 @@ return [
                 // PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
-        'facturacion' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE3', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                // PDO::ATTR_EMULATE_PREPARES => true,
-            ]) : [],
-        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
@@ -131,7 +111,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
     ],
 
     /*
