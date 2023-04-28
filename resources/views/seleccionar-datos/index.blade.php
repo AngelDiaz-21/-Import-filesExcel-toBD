36f85f530608991cc9daec29817d9991c340dd5d
Angel <angelSistemas21@gmail.com>
2023-04-27 18:01:39 -0600

    add: Vista seleccionar-datos

--- resources/views/seleccionar-datos/index.blade.php
+++ resources/views/seleccionar-datos/index.blade.php
@@ -8,62 +8,52 @@
             <div class="col">
                 <h3 class="mb-0">Seleccionar datos</h3>
             </div>
-            {{-- <div class="col text-right">
-                <a href="{{ url('municipio/create') }}" class="btn btn-sm btn-success">Importar nuevos datos</a>
-            </div> --}}
         </div>
     </div>
-    <!-- Para mostrar la notification de agregado correctamente -->
-    <!-- Decimos que si tenemos una variable de sesion llamada notification vamos a mostrar su valor dentro del alert  -->
     <div class="card-body">
-        @if ($errors->any())
-            <div class="alert alert-danger" role="alert">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>
-                        {{ $error }}
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-    
-
         <form action="#" method="post">
             @csrf
-            <div class="form-group">
-                <label for="estado_select">Selecciona un estado</label>  <br>
-                <select class="form-select" aria-label="Default select example" id="estado_select" name="estado_select">
-                    <option disabled selected>Selecciona un estado</option>
-                    @foreach ($estados as $estado)
-                    <option value="{{$estado->clave_Estado}}">{{$estado->nombre_Estado.' - '.$estado->clave_Estado}}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="estado_select">Selecciona un municipio</label> <br>
-                <select name="municipio" id="municipio_select" disabled>
-                    <option disabled selected>Selecciona un municipio</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="estado_select">Selecciona un localidad</label> <br>
-                <select name="localidad" id="localidad_select" disabled>
-                    <option disabled selected>Selecciona una localidad</option>
-                </select>
-            </div>
-            
-            <div class="form-group">
-                <label for="codigoPostal">Codigo postal</label>
-                <input type="text" class="form-control" id="inputCodigoPostal" onkeyup="countCodigoPostal(this);" name="codigoPostal" placeholder="Codigo Postal">
-            </div>
-
-            <div class="form-group">
-                <select name="colonia" id="colonia_select" >
-                    <option disabled selected>Selecciona una colonia</option>
-                </select>
+            <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <label for="estado_select">Selecciona un estado</label>  <br>
+                        <select class="form-select" aria-label="Default select example" id="estado_select" name="estado_select">
+                            <option disabled selected>Selecciona un estado</option>
+                            @foreach ($estados as $estado)
+                            <option value="{{$estado->clave_Estado}}">{{$estado->nombre_Estado.' - '.$estado->clave_Estado}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <label for="estado_select">Selecciona un municipio</label> <br>
+                        <select name="municipio" id="municipio_select" disabled>
+                            <option disabled selected>Selecciona un municipio</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <label for="estado_select">Selecciona un localidad</label> <br>
+                        <select name="localidad" id="localidad_select" disabled>
+                            <option disabled selected>Selecciona una localidad</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <label for="codigoPostal">Codigo postal</label>
+                        <input type="text" class="form-control" id="inputCodigoPostal" onkeyup="countCodigoPostal(this);" name="codigoPostal" placeholder="Codigo Postal">
+                    </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div class="form-group">
+                        <select name="colonia" id="colonia_select" >
+                            <option disabled selected>Selecciona una colonia</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </form>
     </div>
